0d8fbe04988a37ee5327610fdf64c17956165b11
sudais2001 <162806729+sudais2001@users.noreply.github.com>
2024-03-09 18:03:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+// let personName :string = '';
+// personName= prompt('sudais  ') ||''
+// if(personName !== null && personName !=='')
+//      alert(` “Hello ${personName}, would you like to learn some Python today?”`)
+//     else{   
+//          alert('khan')
+//     }
---
+++ app.ts
@@ -0,0 +1,13 @@
+// let personName :string = '';
+
+// personName= prompt('sudais  ') ||''
+
+// if(personName !== null && personName !=='')
+//      alert(` “Hello ${personName}, would you like to learn some Python today?”`)
+//     else{   
+//          alert('khan')
+//     }
+
+
+    
+    
